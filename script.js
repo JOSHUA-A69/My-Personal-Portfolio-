4bd649778c1f402eef95fe65cecb1822355db62c
Joshua Russel Uy <116649829+JOSHUA-A69@users.noreply.github.com>
2024-05-21 22:47:35 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ ScrollReveal().reveal('.home-content h1,.about-img',{origin:'left'});
 ScrollReveal().reveal('.home-content p,.about-content',{origin:'right'});
 
 const typed = new Typed('.multiple-text',{
-    strings:['Information Technology Student at Holy Name University Bohol,Philippines','Software Developer'],
+    strings:['Information Technology Student at Holy Name University Bohol,Philippines','Web Developer'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,

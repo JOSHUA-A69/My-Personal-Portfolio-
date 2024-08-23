b9e021e032b30ae592bcaf3b7d74d69d014f22f6
JOSHUA-A69 <ujoshua368@gmail.com>
2024-08-23 08:52:46 +0800

    modified

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,16 +6,26 @@ const About = () => {
     return (
         <section className="about" id="about">
             <div className="about-img">
-                <img src="https://i.postimg.cc/HW7r5fg5/435397787-434941355886389-7641357581127868885-n.jpg" alt="Personal-Pic"/>
+                <img src="https://i.postimg.cc/HW7r5fg5/435397787-434941355886389-7641357581127868885-n.jpg" alt="Personal-Pic" />
             </div>
             <div className="about-content">
                 <h2 className="heading">About <span>Me</span></h2>
                 <h3>JOSHUA RUSSEL UY</h3>
-                <p>hailing from the picturesque province of Bohol, Philippines. I take pride in my cooperative spirit, resilience, and unwavering determination. I am adept at adapting to diverse situations and maintaining composure under pressure. My approach to challenges involves innovative problem-solving and continuous self-improvement. I am committed to cultivating meaningful relationships, demonstrating empathy, and embracing a calm and thoughtful demeanor. I set daily goals and value authenticity and genuineness in all interactions.</p>
-                <p>My goal is to establish a fulfilling career as a software engineer and web developer, specializing in front-end development. I aspire to contribute my technical skills and creativity to the ever-evolving tech industry, pursuing opportunities that allow me to innovate, solve complex problems, and make a meaningful impact.</p>
+                <p>
+                    Hailing from the picturesque province of Bohol, Philippines, I take pride in my cooperative spirit, resilience, and unwavering determination. 
+                    I am adept at adapting to diverse situations and maintaining composure under pressure. 
+                    My approach to challenges involves innovative problem-solving and continuous self-improvement. 
+                    I am committed to cultivating meaningful relationships, demonstrating empathy, and embracing a calm and thoughtful demeanor. 
+                    I set daily goals and value authenticity and genuineness in all interactions.
+                </p>
+                <p>
+                    My goal is to establish a fulfilling career as a software engineer and web developer, specializing in front-end development. 
+                    I aspire to contribute my technical skills and creativity to the ever-evolving tech industry, pursuing opportunities that allow me to innovate, 
+                    solve complex problems, and make a meaningful impact.
+                </p>
             </div>
         </section>
     );
 }
-
-export default About;
+    
+export default About; 
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import './portfolio.css';
 
-
 const Portfolio = () => {
     return (
         <section className="portfolio" id="portfolio">
             <h2 className="heading">Latest <span>Project</span></h2>
             <div className="portfolio-container">
-            <div className="portfolio-box">
-                    <img src="https://i.postimg.cc/DzqZyfC5/github-logo.png" alt="github"/>
+                <div className="portfolio-box">
+                    <img src="https://i.postimg.cc/pXWcKbQB/coming-soon.png" alt="coming soon" />
+                    <div className="portfolio-layer">
+                        <h4>Coming Soon</h4>
+                        <p>coming soon</p>
+                        <a href=""><i className='fas fa-link'></i></a>
+                    </div>
+                </div>
+                <div className="portfolio-box">
+                    <img src="https://i.postimg.cc/pXWcKbQB/coming-soon.png" alt="coming soon" />
+                    <div className="portfolio-layer">
+                        <h4>Coming Soon</h4>
+                        <p>coming soon</p>
+                        <a href=""><i className='fas fa-link'></i></a>
+                    </div>
+                </div>
+                <div className="portfolio-box">
+                    <img src="https://i.postimg.cc/DzqZyfC5/github-logo.png" alt="github" />
                     <div className="portfolio-layer">
                         <h4>My GitHub Repositories</h4>
                         <p>Feel free to explore my GitHub repositories where you can find most of my showcase projects.</p>
@@ -16,29 +31,9 @@ const Portfolio = () => {
                     </div>
                 </div>
             </div>
-
-                {/* 
-                <div className="portfolio-box">
-                    <img src="https://i.postimg.cc/rFZZbns8/Header.png" alt="RyoukouTravel"/>
-                    <div className="portfolio-layer">
-                        <h4>RyoukouTravel</h4>
-                        <p>a Travel blog website for Bohol Philippines, appreciating on Bohol's most overlooked tourist attractions.</p>
-                        <a href="https://joshua-a69.github.io/RyoukouTravel-website-School-Project/LogInPage.html"><i className='fas fa-link'></i></a>
-                    </div>
-                </div>
-                <div className="portfolio-box">
-                    <img src="https://i.postimg.cc/y6j9Bj5t/SScamera-Shop.png" alt="School-Project-eCom-Website"/>
-                    <div className="portfolio-layer">
-                        <h4>e-Commerce Website</h4>
-                        <p>Front-end e-Commerce Website</p>
-                        <a href="https://joshua-a69.github.io/School-Project-eCom-Website/index.html"><i className='fas fa-link'></i></a>
-                    </div>
-                </div>
-                */}
-                
-                
         </section>
     );
 }
+
 
 export default Portfolio;

255bc5d71ca9f5cf0c3e2907aa7412a7a5efe2c5
JOSHUA-A69 <ujoshua368@gmail.com>
2024-10-14 07:35:50 +0800

    modified

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,28 +7,52 @@ const Portfolio = () => {
         <section className="portfolio" id="portfolio">
             <h2 className="heading">Latest <span>Project</span></h2>
             <div className="portfolio-container">
-                <div className="portfolio-box">
-                <img src={require('../img/coming-soon.png')} alt="coming soon" />
-                    <div className="portfolio-layer">
-                        <h4>Coming Soon</h4>
-                        <p>coming soon</p>
-                        <a href="#"><i className='fas fa-link'></i></a>
-                    </div>
-                </div>
-                <div className="portfolio-box">
-                <img src={require('../img/coming-soon.png')} alt="coming soon" />
-                    <div className="portfolio-layer">
-                        <h4>Coming Soon</h4>
-                        <p>coming soon</p>
-                        <a href="#"><i className='fas fa-link'></i></a>
-                    </div>
-                </div>
-                <div className="portfolio-box">
+            <div className="portfolio-box">
                 <img src={require('../img/GitHub.png')} alt="github" />
                     <div className="portfolio-layer">
                         <h4>My GitHub Repositories</h4>
                         <p>Feel free to explore my GitHub repositories where you can find most of my showcase projects.</p>
                         <a href="https://github.com/JOSHUA-A69?tab=repositories"><i className='fas fa-link'></i></a>
+                    </div>
+                </div>
+                <div className="portfolio-box">
+                <img src={require('../img/coming-soon.png')} alt="coming soon" />
+                    <div className="portfolio-layer">
+                        <h4>Coming Soon</h4>
+                        <p>coming soon</p>
+                        <a href="#"><i className='fas fa-link'></i></a>
+                    </div>
+                </div>
+                <div className="portfolio-box">
+                <img src={require('../img/coming-soon.png')} alt="coming soon" />
+                    <div className="portfolio-layer">
+                        <h4>Coming Soon</h4>
+                        <p>coming soon</p>
+                        <a href="#"><i className='fas fa-link'></i></a>
+                    </div>
+                </div>
+                <div className="portfolio-box">
+                <img src={require('../img/coming-soon.png')} alt="coming soon" />
+                    <div className="portfolio-layer">
+                        <h4>Coming Soon</h4>
+                        <p>coming soon</p>
+                        <a href="#"><i className='fas fa-link'></i></a>
+                    </div>
+                </div>
+                <div className="portfolio-box">
+                <img src={require('../img/coming-soon.png')} alt="coming soon" />
+                    <div className="portfolio-layer">
+                        <h4>Coming Soon</h4>
+                        <p>coming soon</p>
+                        <a href="#"><i className='fas fa-link'></i></a>
+                    </div>
+                </div>
+                <div className="portfolio-box">
+                <img src={require('../img/coming-soon.png')} alt="coming soon" />
+                    <div className="portfolio-layer">
+                        <h4>Coming Soon</h4>
+                        <p>coming soon</p>
+                        <a href="#"><i className='fas fa-link'></i></a>
                     </div>
                 </div>
             </div>

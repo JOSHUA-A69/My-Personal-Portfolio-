e4cabd9824dab97d6ce9a439f5565a0f3ecbf155
JOSHUA-A69 <ujoshua368@gmail.com>
2025-01-01 09:56:53 +0800

    2025

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer-text">
-                <p>Copyright &copy; 2024 by Joshua Russel Uy | All Rights Reserved.</p>
+                <p>Copyright &copy; 2025 by Joshua Russel Uy | All Rights Reserved.</p>
                 <p>Powered by <i className="fab fa-react" style={{ fontSize: '30px' }}></i></p>
             </div>
             <div className="footer-iconTop">
